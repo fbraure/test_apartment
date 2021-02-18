9124d2e2fe9945339a54ba726a80811c003e235f
Florent BRAURE <florent.braure@gmail.com>
2021-02-18 22:58:30 +0100

    delete users from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@ User.destroy_all
 Apartment::Tenant.reset
 
 User.create!(email: 'admin@example.com', password: 'azerty', password_confirmation: 'azerty', admin: true)
-user1 = User.create!(email: 'florent@site1.com', password: 'azerty', password_confirmation: 'azerty', )
-user2 = User.create!(email: 'florent@site2.com', password: 'azerty', password_confirmation: 'azerty', )
 
 4.times do |i|
   Restaurant.create(name: "Restaurant #{i+1}")
