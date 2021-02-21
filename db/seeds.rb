787902b6c72429448a0fa07d9ea269283ea9d481
Florent BRAURE <florent.braure@gmail.com>
2021-02-21 11:41:06 +0100

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 puts "START SEED"
-
+puts ">> Current Tenant: #{Apartment::Tenant.current}"
+puts ">> Tenants already in: #{Apartment.tenant_names}"
 Restaurant.destroy_all
 User.destroy_all
-Apartment::Tenant.reset
+# Apartment::Tenant.reset
+["site1", "site2"].each  do |tenant|
+  Apartment::Tenant.create(tenant) unless Apartment.tenant_names.include?(tenant)
+end
 
 User.create!(email: 'admin@example.com', password: 'azerty', password_confirmation: 'azerty', admin: true)
 
